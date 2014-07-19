cac347350532b2f440121be7318a30c88d68a26e
Silvio Fragnani <silviofragnanisilva@gmail.com>
2014-07-19 12:16:13 -0300

    TestOnlyNDK update

--- TestOnlyNDK/jni/Application.mk
+++ TestOnlyNDK/jni/Application.mk
@@ -1,3 +1,4 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := x86 #armeabi armeabi-v7a x86
+#APP_ABI := x86 #armeabi armeabi-v7a x86
+APP_ABI := all
 APP_PLATFORM := android-8
